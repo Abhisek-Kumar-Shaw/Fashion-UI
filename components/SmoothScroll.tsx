9c88aa10eaf72fb9cd4685f80cce5c48fc52147b
Abhisek-Kumar-Shaw <kumarabhisekshaw1999@gmail.com>
2026-07-19 20:52:17 +0530

    package change

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,28 +1,9 @@
-"use client";
-
-import { ReactLenis } from "@studio-freight/react-lenis";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 interface SmoothScrollProps {
   children: ReactNode;
 }
 
 export default function SmoothScroll({ children }: SmoothScrollProps) {
-  return (
-    <ReactLenis
-      root
-      options={{
-        duration: 1.8,
-        lerp: 0.05,
-        smoothWheel: true,
-        wheelMultiplier: 0.9,
-        touchMultiplier: 1.5,
-        infinite: false,
-        // Premium syrupy easing function
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      }}
-    >
-      {children as ReactNode & string}
-    </ReactLenis>
-  );
+  return <>{children}</>;
 }
